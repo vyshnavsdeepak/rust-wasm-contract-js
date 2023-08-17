8209653f44a63d0073557303d3717266efdd0162
Vyshnav S Deepak <vyshnav.s.deepak@gmail.com>
2023-08-17 16:04:00 +0530

    separate get_return_string fn

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use std::mem;
 use std::ffi::{CString, CStr};
 use std::os::raw::c_void;
 
+static mut COUNTER: u32 = 0;
+
 #[no_mangle]
 pub extern "C" fn alloc() -> *mut c_void {
     let mut buf = Vec::with_capacity(1024);
@@ -19,16 +21,23 @@ pub unsafe extern "C" fn dealloc(ptr: *mut c_void) {
 
 #[no_mangle]
 pub unsafe extern "C" fn greet(ptr: *mut u8) {
+    COUNTER += 1;
     let str_content = CStr::from_ptr(ptr as *const i8).to_str().unwrap();
     let mut string_content = String::from("Hello, ");
 
     string_content.push_str(str_content);
     string_content.push_str("!");
 
+    get_return_string(string_content, ptr);
+}
+
+#[no_mangle]
+unsafe fn get_return_string(string_content: String, ptr: *mut u8) -> () {
     let c_headers = CString::new(string_content).unwrap();
 
     let bytes = c_headers.as_bytes_with_nul();
 
     let header_bytes = std::slice::from_raw_parts_mut(ptr, 1024);
+
     header_bytes[..bytes.len()].copy_from_slice(bytes);
 }
